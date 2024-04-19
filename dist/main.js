851064663a64a48cea85295e8871f0a370106dd8
nekane1117 <november_melody_bell@yahoo.co.jp>
2024-04-19 05:18:21 +0000

    add dist

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// import roleHarvester from "role.harvester";
+// import roleUpgrader from "role.upgrader";
+module.exports.loop = function () {
+    for (const name in Game.creeps) {
+        const creep = Game.creeps[name];
+        if (creep.memory.role == "harvester") {
+            console.log("harvester");
+        }
+    }
+};
