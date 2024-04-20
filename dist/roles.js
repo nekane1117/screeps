c9020546f32a7298d81413993e3e3a510ff28c6c
nekane1117 <november_melody_bell@yahoo.co.jp>
2024-04-20 12:42:06 +0000

    fix filename

--- dist/roles.js
+++ dist/roles.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.behaviors = void 0;
 const role_harvester_1 = __importDefault(require("./role.harvester"));
+const role_upgrader_1 = __importDefault(require("./role.upgrader"));
 exports.behaviors = {
-    harvester: role_harvester_1.default,
+    harvester: role_harvester_1.default, upgrader: role_upgrader_1.default
 };
--- roles.ts
+++ roles.ts
@@ -1,6 +1,8 @@
 import harvester from "./role.harvester";
+import upgrader from "./role.upgrader";
 export type CreepBehavior = (creep: Creeps) => void;
 
 export const behaviors: Partial<Record<ROLES, CreepBehavior>> = {
   harvester,
+  upgrader,
 };
