5fde3051908ee4b8897bf927abbba5cee164a49f
nekane1117 <november_melody_bell@yahoo.co.jp>
2024-06-03 00:37:50 +0900

    auto sell order

--- dist/structure.terminal.js
+++ dist/structure.terminal.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const constants_1 = require("./constants");
+const util_creep_1 = require("./util.creep");
 const utils_1 = require("./utils");
 function behaviors(terminal) {
     (0, utils_1.logUsage)(`terminal:${terminal.room.name}`, () => {
@@ -11,10 +12,18 @@ function behaviors(terminal) {
         if (Game.cpu.bucket < 100) {
             return;
         }
+        const getRemainingTotal = (resourceType) => {
+            return _(Object.values(Game.market.orders))
+                .filter((o) => o.type === ORDER_SELL && o.resourceType === resourceType)
+                .sum((o) => o.remainingAmount);
+        };
+        const getUsedCapacity = (resourceType) => {
+            return terminal.store[resourceType] - getRemainingTotal(resourceType);
+        };
         const { room } = terminal;
         const mineral = _(room.find(FIND_MINERALS)).first();
         mineral &&
-            room.visual.text(`${mineral.mineralType}:${terminal.store[mineral.mineralType]}`, terminal.pos.x, terminal.pos.y - 1, {
+            room.visual.text(`${mineral.mineralType}:${getUsedCapacity(mineral.mineralType)}(${getRemainingTotal(mineral.mineralType)})`, terminal.pos.x, terminal.pos.y - 1, {
                 align: "left",
             });
         if (terminal.cooldown) {
@@ -26,15 +35,49 @@ function behaviors(terminal) {
                 return Game.market.deal(order.id, order.amount, terminal.room.name);
             }
         }
+        if (mineral && getUsedCapacity("energy") > room.energyCapacityAvailable && getUsedCapacity(mineral.mineralType) > constants_1.TERMINAL_THRESHOLD * 2) {
+            const history = _(Game.market.getHistory(mineral.mineralType)).last();
+            if (history) {
+                const buyOrder = _(Game.market.getAllOrders({ type: ORDER_BUY, resourceType: mineral.mineralType }))
+                    .filter((o) => o.roomName && o.price >= history.avgPrice)
+                    .max((o) => o.price);
+                if (!_.isNumber(buyOrder)) {
+                    return console.log("deal", util_creep_1.RETURN_CODE_DECODER[Game.market
+                        .deal(buyOrder.id, Math.min(buyOrder.remainingAmount, (0, utils_1.calcMaxTransferAmount)(buyOrder, terminal), getUsedCapacity(mineral.mineralType)), room.name)
+                        .toString()], JSON.stringify(buyOrder));
+                }
+                else {
+                    const minSellOrder = _(Game.market.getAllOrders({ type: ORDER_SELL, resourceType: mineral.mineralType }))
+                        .filter((o) => o.roomName && o.price >= history.avgPrice)
+                        .min((o) => o.price);
+                    if (minSellOrder) {
+                        const price = minSellOrder.price - 0.01;
+                        const totalAmount = Math.min(getUsedCapacity(mineral.mineralType) - constants_1.TERMINAL_THRESHOLD, Math.floor(Game.market.credits / price / 0.05));
+                        if (totalAmount > 1000) {
+                            return Game.market.createOrder({
+                                type: ORDER_SELL,
+                                resourceType: mineral.mineralType,
+                                price,
+                                totalAmount: totalAmount,
+                                roomName: terminal.room.name,
+                            });
+                        }
+                    }
+                }
+            }
+            else {
+                console.log("no history");
+            }
+        }
         for (const resourceType of Object.keys(terminal.store).filter((resourceType) => {
-            return resourceType[0] === resourceType[0].toUpperCase() && resourceType.length >= 2 && terminal.store[resourceType] > 1000;
+            return resourceType[0] === resourceType[0].toUpperCase() && resourceType.length >= 2 && getUsedCapacity(resourceType) > 1000;
         })) {
             const avg = ((_a = _(Game.market.getHistory(resourceType)).last()) === null || _a === void 0 ? void 0 : _a.avgPrice) || Infinity;
             const order = _(Game.market.getAllOrders({ type: ORDER_BUY, resourceType }))
                 .filter((o) => o.price >= avg)
                 .max((o) => o.price);
             if (order) {
-                const result = Game.market.deal(order.id, Math.min(order.amount, terminal.store[resourceType] - 1000), terminal.room.name);
+                const result = Game.market.deal(order.id, Math.min(order.amount, getUsedCapacity(resourceType) - 1000), terminal.room.name);
                 if (result === OK) {
                     return;
                 }
--- structure.terminal.ts
+++ structure.terminal.ts
@@ -1,4 +1,5 @@
 import { TERMINAL_THRESHOLD } from "./constants";
+import { RETURN_CODE_DECODER } from "./util.creep";
 import { calcMaxTransferAmount, logUsage } from "./utils";
 
 export default function behaviors(terminal: Structure) {
@@ -11,14 +12,30 @@ export default function behaviors(terminal: Structure) {
       return;
     }
 
+    const getRemainingTotal = (resourceType: ResourceConstant) => {
+      return _(Object.values(Game.market.orders))
+        .filter((o) => o.type === ORDER_SELL && o.resourceType === resourceType)
+        .sum((o) => o.remainingAmount);
+    };
+
+    const getUsedCapacity = (resourceType: ResourceConstant) => {
+      // 実際持ってる量に売り注文の合計を引いたものを返す
+      return terminal.store[resourceType] - getRemainingTotal(resourceType);
+    };
+
     const { room } = terminal;
 
     const mineral = _(room.find(FIND_MINERALS)).first();
 
     mineral &&
-      room.visual.text(`${mineral.mineralType}:${terminal.store[mineral.mineralType]}`, terminal.pos.x, terminal.pos.y - 1, {
-        align: "left",
-      });
+      room.visual.text(
+        `${mineral.mineralType}:${getUsedCapacity(mineral.mineralType)}(${getRemainingTotal(mineral.mineralType)})`,
+        terminal.pos.x,
+        terminal.pos.y - 1,
+        {
+          align: "left",
+        },
+      );
 
     // ここから購入処理なのでcooldown有るときは終わる
     if (terminal.cooldown) {
@@ -34,28 +51,65 @@ export default function behaviors(terminal: Structure) {
       }
     }
 
-    // // 自室のミネラルをため込んでるときはとにかく売る
-    // if (mineral && terminal.store.energy > room.energyCapacityAvailable && terminal.store[mineral.mineralType] > TERMINAL_THRESHOLD) {
-    //   // 一番高く買ってくれるオーダー
-    //   const order = _(Game.market.getAllOrders({ type: ORDER_BUY, resourceType: mineral.mineralType }))
-    //     // 部屋名がないのはよくわからないので無視する
-    //     .filter((o) => o.roomName)
-    //     .max((o) => o.price);
+    // 自室のミネラルをため込んでるときはとにかく売る
+    if (mineral && getUsedCapacity("energy") > room.energyCapacityAvailable && getUsedCapacity(mineral.mineralType) > TERMINAL_THRESHOLD * 2) {
+      // 一番高く買ってくれるオーダー
+      const history = _(Game.market.getHistory(mineral.mineralType)).last();
+      if (history) {
+        // 平均より高い額の買い注文があるか
+        const buyOrder = _(Game.market.getAllOrders({ type: ORDER_BUY, resourceType: mineral.mineralType }))
+          // 部屋名がないのはよくわからないので無視する
+          .filter((o) => o.roomName && o.price >= history.avgPrice)
+          .max((o) => o.price) as unknown as Order | number;
 
-    //   // 今のエネルギーで支払える最大量
-    //   if (order) {
-    //     return Game.market.deal(
-    //       order.id,
-    //       // オーダーの残量、支払える上限、保有上限の中で一番少ない分売る
-    //       Math.min(order.remainingAmount, calcMaxTransferAmount(order, terminal), terminal.store[mineral.mineralType]),
-    //       room.name,
-    //     );
-    //   }
-    // }
+        // 有る場合は今のエネルギーで支払える最大量売る
+        if (!_.isNumber(buyOrder)) {
+          return console.log(
+            "deal",
+            RETURN_CODE_DECODER[
+              Game.market
+                .deal(
+                  buyOrder.id,
+                  // オーダーの残量、支払える上限、保有上限の中で一番少ない分売る
+                  Math.min(buyOrder.remainingAmount, calcMaxTransferAmount(buyOrder, terminal), getUsedCapacity(mineral.mineralType)),
+                  room.name,
+                )
+                .toString()
+            ],
+            JSON.stringify(buyOrder),
+          );
+        } else {
+          // 買い注文が無いとき
+
+          // 平均値より高い中で一番安い注文を探す
+          const minSellOrder = _(Game.market.getAllOrders({ type: ORDER_SELL, resourceType: mineral.mineralType }))
+            // 部屋名がないのはよくわからないので無視する
+            .filter((o) => o.roomName && o.price >= history.avgPrice)
+            .min((o) => o.price);
+
+          if (minSellOrder) {
+            const price = minSellOrder.price - 0.01;
+            const totalAmount = Math.min(getUsedCapacity(mineral.mineralType) - TERMINAL_THRESHOLD, Math.floor(Game.market.credits / price / 0.05));
+            if (totalAmount > 1000) {
+              return Game.market.createOrder({
+                type: ORDER_SELL,
+                resourceType: mineral.mineralType,
+                price,
+                // 保有量か支払手数料で払える額の少ないほう
+                totalAmount: totalAmount,
+                roomName: terminal.room.name,
+              });
+            }
+          }
+        }
+      } else {
+        console.log("no history");
+      }
+    }
 
     for (const resourceType of (Object.keys(terminal.store) as (MineralConstant | MineralCompoundConstant)[]).filter((resourceType) => {
       // とりあえず1000以上ある化合物
-      return resourceType[0] === resourceType[0].toUpperCase() && resourceType.length >= 2 && terminal.store[resourceType] > 1000;
+      return resourceType[0] === resourceType[0].toUpperCase() && resourceType.length >= 2 && getUsedCapacity(resourceType) > 1000;
     })) {
       // const ingredients = REVERSE_REACTIONS[resourceType];
       // if (!ingredients) {
@@ -80,7 +134,7 @@ export default function behaviors(terminal: Structure) {
         .filter((o) => o.price >= avg)
         .max((o) => o.price);
       if (order) {
-        const result = Game.market.deal(order.id, Math.min(order.amount, terminal.store[resourceType] - 1000), terminal.room.name);
+        const result = Game.market.deal(order.id, Math.min(order.amount, getUsedCapacity(resourceType) - 1000), terminal.room.name);
         if (result === OK) {
           return;
         }
