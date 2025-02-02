1bcbe45a5300575d56c884f6a03156aecc8ef0fa
Nekane1117 <november_melody_bell@yahoo.co.jp>
2025-02-02 11:38:00 +0900

    flagの基準変更

--- role.claimer.ts
+++ role.claimer.ts
@@ -6,6 +6,7 @@ const behavior: CreepBehavior = (claimer: Creeps) => {
   if (!isClaimer(claimer)) {
     return console.log(`${claimer.name} is not Builder`);
   }
+
   const moveMeTo = (target: RoomPosition | _HasRoomPosition) => customMove(claimer, target, {});
 
   const flag = Game.flags[claimer.memory.flagName];
