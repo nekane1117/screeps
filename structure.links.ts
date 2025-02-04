d8edd065ceb8e7afdc0f0bb0ce376079f2c0c754
Nekane1117 <november_melody_bell@yahoo.co.jp>
2025-02-04 13:01:14 +0900

    コントローラー用Link

--- structure.links.ts
+++ structure.links.ts
@@ -1,4 +1,5 @@
 import { getMainSpawn } from "./util.creep";
+import { findMyStructures, getCapacityRate } from "./utils";
 export default function behavior(links: StructureLink[]) {
   const room = _.first(links)?.room;
   const center = room && (room.storage || getMainSpawn(room));
@@ -6,8 +7,14 @@ export default function behavior(links: StructureLink[]) {
     return;
   }
 
+  // コントローラー隣接リンク
+  const controllerLink = findMyStructures(room).link.find((l) => room.controller && l.pos.inRangeTo(room.controller.pos, 3));
+
   // 中心地に近い順に並べ替える
   const [centerLink, ...tail] = _(links)
+    .filter((l) => {
+      return l.id !== controllerLink?.id;
+    })
     .sortBy((l) => {
       return l.pos.getRangeTo(center);
     })
@@ -18,4 +25,8 @@ export default function behavior(links: StructureLink[]) {
       l.transferEnergy(centerLink, _.floor(Math.min(l.store.energy, centerLink.store.getFreeCapacity(RESOURCE_ENERGY)), -2));
     }
   });
+
+  if (getCapacityRate(centerLink) > 0.5 && controllerLink) {
+    centerLink.transferEnergy(controllerLink, _.floor(Math.min(centerLink.store.energy, controllerLink.store.getFreeCapacity(RESOURCE_ENERGY)), -2));
+  }
 }
