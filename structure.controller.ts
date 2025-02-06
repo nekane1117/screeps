2e8ad09e1d8c2289dfb37d7d14dcf46e10f0d27a
Nekane1117 <november_melody_bell@yahoo.co.jp>
2025-02-07 00:41:47 +0900

    reset upgrader

--- structure.controller.ts
+++ structure.controller.ts
@@ -92,8 +92,8 @@ function updateUpgraderSize(room: Room) {
 }
 
 function getUpgraderBody(room: Room): BodyPartConstant[] {
-  // きゃりーサイズ * 係数 / 2(2個単位で入れるので) * 2(変化率)
-  const requestSize = _.ceil((room.memory.carrySize?.upgrader || 1) * 2);
+  // きゃりーサイズ * 係数 / 2(2個単位で入れるので)
+  const requestSize = _.ceil(((room.memory.carrySize?.upgrader || 1) * 2) / 2);
 
   let totalCost = 0;
 
